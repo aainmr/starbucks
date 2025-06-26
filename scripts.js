b0d86b7446d2e0d9d1c35a33c4814ebf47889042
aainmr <saltorisfrontend@gmail.com>
2025-06-26 00:01:35 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,3 +3,18 @@ let circle = document.querySelector(".circle")
 function changeColor(color){
 circle.style.background = color
 }
+
+let circle = document.querySelector(".circle");
+let mainImg = document.querySelector(".starbucks");
+let buttons = document.querySelectorAll(".menu_btn");
+
+function changeColor(color) {
+  circle.style.background = color;
+}
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const imgPath = button.getAttribute("data-img");
+    mainImg.src = imgPath;
+  });
+});
