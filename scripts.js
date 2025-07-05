7980bf0965e565e73beb5e2031590f626cfa98ee
aainmr <saltorisfrontend@gmail.com>
2025-07-05 20:15:50 -0300

    aa

--- scripts.js
+++ scripts.js
@@ -3,9 +3,6 @@ let mainImg = document.querySelector(".starbucks");
 let buttons = document.querySelectorAll(".menu_btn");
 
 function changeColor(color) {
-  circle.style.background = color;
-}
-function changeTheme(imgPath, color) {
-  document.querySelector('.starbucks').src = imgPath;
   document.querySelector('.circle').style.background = color;
+  document.querySelector('.starbucks').src = 'img/img1.png'; // ou img2.png, etc.
 }
